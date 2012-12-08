07dc0e099069a9cc544cbfda5baf481fc31270f8
zen <innovationlife.net@gmail.com>
2012-12-09 03:35:46 +0900

    Add Create Feature

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,18 @@ class PostsController < ApplicationController
 	def show
 		@post = Post.find(params[:id])
 	end
+
+	def new
+		@post = Post.new
+	end
+
+	def create
+		@post = Post.new(params[:id])	
+
+		if @post.save
+			redirect_to posts_path
+		else
+			render action: 'new'	
+		end
+	end
 end
